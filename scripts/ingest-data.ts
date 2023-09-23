f69abe28921c50a692a8215dff2a33ea9b7a8b4d
hollistic <hbrewer27@gmail.com>
2023-09-23 15:45:51 -0400

    'asdf'

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -6,7 +6,7 @@ import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 
-/* Name of directory to retrieve your files from 
+/* Name of directory to retrieve your files from
    Make sure to add your PDF files inside the 'docs' folder
 */
 const filePath = 'docs';
